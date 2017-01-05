4d0657577feef4ad790c07a87825515e34467a39
Sacha Schutz <sschutz@med-laennec.univ-brest.fr>
2017-01-05 10:53:39 +0100

    errors handle

--- app/api/projects.py
+++ app/api/projects.py
@@ -1,9 +1,11 @@
 from flask import jsonify, request, g, abort, url_for, current_app 
-from . import api
+from app.api import api
+from app.exceptions import CustomError
 
 
 @api.route('/projects')
 def get_projects():
-    return jsonify(["yes","yo", current_app.config['DATA_PATH']])
+	raise CustomError("sorry ")
+    # return jsonify(["yes","yo", current_app.config['DATA_PATH']])
 
 
